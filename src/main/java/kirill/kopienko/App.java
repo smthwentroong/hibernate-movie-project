558aadb290ca61fac6fffcf0ace566427669539d
smthwentroong <kirill.kopienko@gmail.com>
2024-02-04 23:23:27 +0300

    Initial commit

---
+++ src/main/java/kirill/kopienko/App.java
@@ -0,0 +1,13 @@
+package kirill.kopienko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
