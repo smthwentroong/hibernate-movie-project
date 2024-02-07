502e7220ece6d318ecb01f4c1fb5961af7e23250
smthwentroong <kirill.kopienko@gmail.com>
2024-02-07 21:13:05 +0300

    initialized all DAO instances and added find-by-name method to CityDAO.java

--- src/main/java/kirill/kopienko/App.java
+++ src/main/java/kirill/kopienko/App.java
@@ -1,13 +1,52 @@
 package kirill.kopienko;
 
-/**
- * Hello world!
- *
- */
+import kirill.kopienko.dao.*;
+import kirill.kopienko.entities.*;
+import kirill.kopienko.utils.MySessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static final ActorDAO actorDAO;
+    public static final AddressDAO addressDAO;
+    public static final CategoryDAO categoryDAO;
+    public static final CityDAO cityDAO;
+    public static final CountryDAO countryDAO;
+    public static final CustomerDAO customerDAO;
+    public static final FilmDAO filmDAO;
+    public static final FilmTextDAO filmTextDAO;
+    public static final InventoryDAO inventoryDAO;
+    public static final LanguageDAO languageDAO;
+    public static final PaymentDAO paymentDAO;
+    public static final RentalDAO rentalDAO;
+    public static final StaffDAO staffDAO;
+    public static final StoreDAO storeDAO;
 
+    static {
+        actorDAO = new ActorDAO(Actor.class);
+        addressDAO = new AddressDAO(Address.class);
+        categoryDAO = new CategoryDAO(Category.class);
+        cityDAO = new CityDAO(City.class);
+        countryDAO = new CountryDAO(Country.class);
+        customerDAO = new CustomerDAO(Customer.class);
+        filmDAO = new FilmDAO(Film.class);
+        filmTextDAO = new FilmTextDAO(FilmText.class);
+        inventoryDAO = new InventoryDAO(Inventory.class);
+        languageDAO = new LanguageDAO(Language.class);
+        paymentDAO = new PaymentDAO(Payment.class);
+        rentalDAO = new RentalDAO(Rental.class);
+        staffDAO = new StaffDAO(Staff.class);
+        storeDAO = new StoreDAO(Store.class);
+    }
+    public static void main( String[] args ) {
+
+    }
+
+    public static void createCustomer() {
+        try (Session session = MySessionFactory.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+        }
     }
 }
--- src/main/java/kirill/kopienko/dao/CityDAO.java
+++ src/main/java/kirill/kopienko/dao/CityDAO.java
@@ -1,9 +1,16 @@
 package kirill.kopienko.dao;
 
 import kirill.kopienko.entities.City;
+import org.hibernate.query.Query;
 
 public class CityDAO extends GenericDAO<City> {
     public CityDAO(Class<City> classToSet) {
         super(classToSet);
+    }
+
+    public City getByName(String name) {
+        Query<City> query = getCurrentSession().createQuery("select c from City c where c.city = :name", City.class);
+        query.setParameter("name", name);
+        return query.getSingleResult();
     }
 }
